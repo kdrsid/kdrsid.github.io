2a50d4ba42e0592345a4c22ad6df253642781111
kdrsid <mpl6@sfu.ca>
2025-06-19 18:09:36 -0700

    added phone and mail icon in contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Contact.css';
+import { FiPhoneCall } from "react-icons/fi";
+import { IoMailOutline } from "react-icons/io5";
 const Contact = () => {
   return (
     <section id="contact" className="section">
         <h1>Contact Me</h1>
         <h2>Get in touch!</h2>
-        <p>Phone number: +1 236-258-6381</p>
-        <p>Mail: kris68008@gmail.com</p>
+        <p><FiPhoneCall color='lightgray'/> Phone number: +1 236-258-6381</p>
+        <p><IoMailOutline color='lightgray'/> Mail: kris68008@gmail.com</p>
     </section>
   )
 }
